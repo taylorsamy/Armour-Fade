bdd8adaea75af7604667639e704fcfa4c7bc2c2b
Taylor Amy <taylorsamy@users.noreply.github.com>
2016-01-15 21:57:19 -0700

    Updated command structure Added global prefix fixed armour deletion bug Enchantments are no longer removed when the colour changes Cleaned up code

--- src/main/java/com/relicpvp/devteam/ArmourFade.java
+++ src/main/java/com/relicpvp/devteam/ArmourFade.java
@@ -1,6 +1,8 @@
+/**
+ * @author Taylorsamy
+ * 
+ * **/
 package com.relicpvp.devteam;
-
-import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -18,16 +20,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ArmourFade extends JavaPlugin implements Listener {
 
+    private static final String PREFIX = ChatColor.DARK_GRAY + "[" + ChatColor.GRAY + "ArmourFade" + ChatColor.DARK_GRAY + "]" + ChatColor.DARK_GREEN + " ";
     int r = 255;
     int g = 0;
     int b = 0;
-    int time = 59;
 
     @Override
     public void onEnable() {
         getLogger().info("ArmourFade Actually Works! :D");
         Bukkit.getPluginManager().registerEvents(this, this);
-        makeClockAndChangingTimers();
+        colourChange();
     }
 
     @Override
@@ -35,18 +37,11 @@ public final class ArmourFade extends JavaPlugin implements Listener {
         getLogger().info("Looks like the server is shutting down... I guess that means I should too.");
     }
 
-    public void makeClockAndChangingTimers() {
+    public void colourChange() {
         Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
 
             @Override
             public void run() {
-
-//red = 255, green ++,
-// green = 255,r --
-//  r = 0, b ++
-// b = 255, g --
-// g = 0, r ++
-// r = 255, b --
 
                 if (r == 255 && b == 0 && g != 255) {
                     g += 10;
@@ -85,43 +80,38 @@ public final class ArmourFade extends JavaPlugin implements Listener {
                     }
                 }
 
-
                 Color c = Color.fromRGB(r, g, b);
-                Player[] arrayOfPlayer;
-                int j = (arrayOfPlayer = Bukkit.getServer().getOnlinePlayers()).length;
-                for (int i = 0; i < j; i++) {
-                    Player p = arrayOfPlayer[i];
-                    try {
-                        if (p.getInventory().getHelmet() != null && (p.getInventory().getHelmet().getItemMeta()
-                                .getDisplayName().equalsIgnoreCase(ChatColor.DARK_BLUE + "rainbow helmet"))) {
-                            p.getInventory().setHelmet(getColorArmor(Material.LEATHER_HELMET, c, "Rainbow Helmet"));
-                        }
-                        if ((p.getInventory().getChestplate() != null)
-                                && (p.getInventory().getChestplate().getItemMeta().getDisplayName()
-                                .equalsIgnoreCase(ChatColor.DARK_BLUE + "rainbow chestplate"))) {
-                            p.getInventory()
-                                    .setChestplate(getColorArmor(Material.LEATHER_CHESTPLATE, c, "Rainbow Chestplate"));
-                        }
-                        if ((p.getInventory().getLeggings() != null) && (p.getInventory().getLeggings().getItemMeta()
-                                .getDisplayName().equalsIgnoreCase(ChatColor.DARK_BLUE + "rainbow leggings"))) {
-                            p.getInventory()
-                                    .setLeggings(getColorArmor(Material.LEATHER_LEGGINGS, c, "Rainbow Leggings"));
-                        }
-                        if ((p.getInventory().getBoots() != null) && (p.getInventory().getBoots().getItemMeta()
-                                .getDisplayName().equalsIgnoreCase(ChatColor.DARK_BLUE + "rainbow boots"))) {
-                            p.getInventory().setBoots(getColorArmor(Material.LEATHER_BOOTS, c, "Rainbow Boots"));
-                        }
-                    } catch (NullPointerException e) {
-
-                    }
-                }
+                checkInventory(c);
             }
         }, 0L, 1L);
     }
 
-    public ItemStack getColorArmor(Material m, Color c, String displayName) {
+    private void checkInventory(Color c) {
+        Player[] arrayOfPlayer;
+        int j = (arrayOfPlayer = Bukkit.getServer().getOnlinePlayers()).length;
+        for (int i = 0; i < j; i++) {
+            Player p = arrayOfPlayer[i];
+            try {
+                if (p.getInventory().getHelmet() != null && (p.getInventory().getHelmet().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.DARK_BLUE + "rainbow helmet"))) {
+                    p.getInventory().setHelmet(setArmourMeta(Material.LEATHER_HELMET, c, "Rainbow Helmet", (LeatherArmorMeta) p.getInventory().getHelmet().getItemMeta()));
+                }
+                if ((p.getInventory().getChestplate() != null) && (p.getInventory().getChestplate().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.DARK_BLUE + "rainbow chestplate"))) {
+                    p.getInventory().setChestplate(setArmourMeta(Material.LEATHER_CHESTPLATE, c, "Rainbow Chestplate", (LeatherArmorMeta) p.getInventory().getChestplate().getItemMeta()));
+                }
+                if ((p.getInventory().getLeggings() != null) && (p.getInventory().getLeggings().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.DARK_BLUE + "rainbow leggings"))) {
+                    p.getInventory().setLeggings(setArmourMeta(Material.LEATHER_LEGGINGS, c, "Rainbow Leggings", (LeatherArmorMeta) p.getInventory().getLeggings().getItemMeta()));
+                }
+                if ((p.getInventory().getBoots() != null) && (p.getInventory().getBoots().getItemMeta().getDisplayName().equalsIgnoreCase(ChatColor.DARK_BLUE + "rainbow boots"))) {
+                    p.getInventory().setBoots(setArmourMeta(Material.LEATHER_BOOTS, c, "Rainbow Boots", (LeatherArmorMeta) p.getInventory().getBoots().getItemMeta()));
+                }
+            } catch (NullPointerException e) {
+
+            }
+        }
+    }
+
+    public ItemStack setArmourMeta(Material m, Color c, String displayName, LeatherArmorMeta meta) {
         ItemStack i = new ItemStack(m, 1);
-        LeatherArmorMeta meta = (LeatherArmorMeta) i.getItemMeta();
         meta.setDisplayName(ChatColor.DARK_BLUE + displayName);
         meta.setColor(c);
         i.setItemMeta(meta);
@@ -131,12 +121,11 @@ public final class ArmourFade extends JavaPlugin implements Listener {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("afother")) {
+
             if (args.length == 1) {
                 Player target = (Bukkit.getServer().getPlayer(args[0]));
                 if (target == null) {
-                    sender.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GRAY + "ArmorFade" + ChatColor.DARK_GRAY
-                            + "]" + ChatColor.DARK_GREEN + args[0]
-                            + " is not online... Did you think that would work?");
+                    sender.sendMessage(PREFIX + args[0] + " is not online... Did you think that would work?");
                     return false;
                 } else {
                     PlayerInventory otherinventory = target.getInventory();
@@ -156,24 +145,32 @@ public final class ArmourFade extends JavaPlugin implements Listener {
                     ItemMeta bootsmeta = boots.getItemMeta();
                     bootsmeta.setDisplayName(ChatColor.DARK_BLUE + "Rainbow Boots");
                     boots.setItemMeta(bootsmeta);
-                    otherinventory.addItem(helmet);
-                    otherinventory.addItem(chest);
-                    otherinventory.addItem(legs);
-                    otherinventory.addItem(boots);
-                    sender.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GRAY + "ArmorFade" + ChatColor.DARK_GRAY
-                            + "]" + ChatColor.DARK_GREEN + args[0] + " has been given rainbow armor!");
-                    target.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GRAY + "ArmorFade" + ChatColor.DARK_GRAY
-                            + "]" + ChatColor.DARK_GREEN + " You have been given rainbow armor!");
+
+                    boolean success = false;
+                    if (otherinventory.getHelmet() == null && otherinventory.getChestplate() == null && otherinventory.getLeggings() == null && otherinventory.getBoots() == null) {
+                        success = true;
+                    } else {
+                        sender.sendMessage(PREFIX + target.getName() + " needs to remove their current armour first.");
+                        target.sendMessage(PREFIX + sender.getName() + " tried to give you Rainbow Armour, but first you need to remove your current armour!");
+                    }
+
+                    if (success) {
+
+                        otherinventory.setHelmet(helmet);
+                        otherinventory.setChestplate(chest);
+                        otherinventory.setLeggings(legs);
+                        otherinventory.setBoots(boots);
+                        sender.sendMessage(PREFIX + target.getName() + " has been given Rainbow Armour!");
+                        target.sendMessage(PREFIX + sender.getName() + " gave you Rainbow Armour!");
+                    }
                 }
             } else {
-                sender.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GRAY + "ArmorFade" + ChatColor.DARK_GRAY + "]"
-                        + ChatColor.DARK_GREEN + " Please specify a player");
+                sender.sendMessage(PREFIX + "Please specify a player");
             }
             return true;
         } else if (cmd.getName().equalsIgnoreCase("afself")) {
             if (!(sender instanceof Player)) {
-                sender.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GRAY + "ArmorFade" + ChatColor.DARK_GRAY + "]"
-                        + ChatColor.DARK_GREEN + " Did you actually try to give the console rainbow armor?");
+                sender.sendMessage(PREFIX + "Did you actually try to give the console Rainbow Armour?");
             } else {
                 Player player = (Player) sender;
                 PlayerInventory inventory = player.getInventory();
@@ -193,17 +190,26 @@ public final class ArmourFade extends JavaPlugin implements Listener {
                 ItemMeta bootsmeta = boots.getItemMeta();
                 bootsmeta.setDisplayName(ChatColor.DARK_BLUE + "Rainbow Boots");
                 boots.setItemMeta(bootsmeta);
-                inventory.addItem(helmet);
-                inventory.addItem(chest);
-                inventory.addItem(legs);
-                inventory.addItem(boots);
-                sender.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GRAY + "ArmorFade" + ChatColor.DARK_GRAY + "]"
-                        + ChatColor.DARK_GREEN + " You gave yourself rainbow armor!");
+
+                boolean success = false;
+                if (inventory.getHelmet() == null && inventory.getChestplate() == null && inventory.getLeggings() == null && inventory.getBoots() == null) {
+                    success = true;
+                } else {
+                    sender.sendMessage(PREFIX + "Please remove your current armour first");
+                }
+                if (success) {
+                    inventory.setHelmet(helmet);
+                    inventory.setChestplate(chest);
+                    inventory.setLeggings(legs);
+                    inventory.setBoots(boots);
+
+                    sender.sendMessage(PREFIX + "You gave yourself Rainbow Armour!");
+                }
             }
             return true;
         } else if (cmd.getName().equalsIgnoreCase("afinfo")) {
-            sender.sendMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GRAY + "ArmorFade" + ChatColor.DARK_GRAY + "]"
-                    + ChatColor.DARK_GREEN + " Plugin created superminer10 and Taylorsamy for the Relic PVP Server");
+            sender.sendMessage(PREFIX + "Plugin created superminer10 and Taylorsamy for the Relic PVP Server");
+            return true;
         }
         return false;
     }
